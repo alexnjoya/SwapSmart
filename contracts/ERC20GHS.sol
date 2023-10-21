6390b092f643b28acf7691e78eb618b86701fefa
alexnjoya <njoyaalexander71@gmail.com>
2023-10-21 14:36:18 +0000

    swap smart

--- contracts/ERC20GHS.sol
+++ contracts/ERC20GHS.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BooToken is ERC20{
 
-    constructor() ERC20("GH", "GHS"){
+    constructor() ERC20("GHANACEDIS", "GHS"){
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
