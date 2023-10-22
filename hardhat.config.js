d78539153792a114c4bc977ae43f4637771c086f
alexnjoya <njoyaalexander71@gmail.com>
2023-10-22 01:38:56 +0000

    swap smart

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,50 +1,16 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: {
-//     compilers: [
-//       {
-//         version: "0.7.6",
-//         settings: {
-//           evmVersion: "istanbul",
-//           optimizer: {
-//             enabled: true,
-//             runs: 1000,
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   networks: {
-//     hardhat: {
-//       forking: {
-//         url: "your",
-//       },
-//     },
-//   },
-// };
-
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  solidity: {
-    version: "0.7.6",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 5000,
-        details: { yul: false },
-      },
-    },
-  },
+  solidity: "0.7.6",
   networks: {
-    hardhat: {
-      chainId: 1337,
-      forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/pim-bid8xGGmQAp2Nxu324feMReP8Sbu",
-        // accounts: [`0x${"your"}`],
-      },
+    hardhat: {},
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      chainId: 137,
+      accounts: [`0x${PRIVATE_KEY}`],
     },
   },
 };
